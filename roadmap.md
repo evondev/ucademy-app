597ca98a8489f10eb9b2cdeca8eaf849b66b1a9a
evondev <trananhtuan400@gmail.com>
2024-06-10 22:52:58 +0700

    create user model

--- roadmap.md
+++ roadmap.md
@@ -8,3 +8,26 @@
 - Protect route
 - Shadcn
 - Darkmode/lightmode
+- User database
+- Schema
+- User actions
+- Create vercel account
+- Deploy vercel
+- Webhook account
+- Server actions
+- Shadcn Form
+- Zod
+- Form Schema
+- Toastify
+- Validation
+- Create course
+- Update course
+- Upload image with Uploadthing
+- Information course
+- Shadcn select
+- Course status
+- Global constant button
+- Button loading
+- Course manage
+- Course details
+- Course delete
---
+++ src/database/user.model.ts
@@ -0,0 +1,53 @@
+import { EUserRole, EUserStatus } from "@/types/enums";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email_address: string;
+  avatar: string;
+  courses: Schema.Types.ObjectId[];
+  status: EUserStatus;
+  role: EUserRole;
+  createdAt: Date;
+}
+const userSchema = new Schema<IUser>({
+  clerkId: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  email_address: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  courses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  role: {
+    type: String,
+    enum: Object.values(EUserRole),
+    default: EUserRole.USER,
+  },
+  status: {
+    type: String,
+    enum: Object.values(EUserStatus),
+    default: EUserStatus.ACTIVE,
+  },
+});
+const User = models.User || model<IUser>("User", userSchema);
+export default User;
