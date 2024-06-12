f07273468d5b6bdb93079d8e551891f6a5c9d8c2
evondev <trananhtuan400@gmail.com>
2024-06-12 22:52:27 +0700

    update sidebar ui

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { menuItems } from "@/constants";
 import { TMenuItem } from "@/types";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import Link from "next/link";
 import { ActiveLink } from "../common";
 import { ModeToggle } from "../common/ModeToggle";
+import { IconUsers } from "../icons";
 
 const Sidebar = () => {
+  const { userId } = useAuth();
   return (
     <div className="p-5 border-r border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker flex flex-col">
       <a
@@ -26,8 +30,16 @@ const Sidebar = () => {
       </ul>
       <div className="mt-auto flex items-center justify-end gap-2">
         <ModeToggle></ModeToggle>
-
-        <UserButton />
+        {!userId ? (
+          <Link
+            href="/sign-in"
+            className="size-10 rounded-lg bg-primary text-white flex items-center justify-center p-1"
+          >
+            <IconUsers />
+          </Link>
+        ) : (
+          <UserButton />
+        )}
       </div>
     </div>
   );
