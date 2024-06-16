cc7458e989d06405581d34443402790229b669e6
evondev <trananhtuan400@gmail.com>
2024-06-16 23:42:53 +0700

    fix study page

--- src/app/(dashboard)/study/page.tsx
+++ src/app/(dashboard)/study/page.tsx
@@ -1,16 +1,14 @@
-import { CourseGrid } from "@/components/common";
-import CourseItem from "@/components/course/CourseItem";
 import Heading from "@/components/typography/Heading";
 
 const page = () => {
   return (
     <>
       <Heading>Khu vực học tập</Heading>
-      <CourseGrid>
+      {/* <CourseGrid>
         <CourseItem></CourseItem>
         <CourseItem></CourseItem>
         <CourseItem></CourseItem>
-      </CourseGrid>
+      </CourseGrid> */}
     </>
   );
 };
